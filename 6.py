a5a8eed6653fd5e6e65804ce7f25bf208826ba7b
Zainab <Zsb5121@rit.edu>
2025-09-09 08:54:16 +0400

    2 files to be commited in pushthenpull

---
+++ 6.py
@@ -0,0 +1,11 @@
+def example(arg1,arg2):
+    
+    arg3 = arg1+arg2
+    arg3=20
+    print(arg3)
+
+arg1=10
+arg2=20
+
+output=example(arg2,arg1)
+print(output)
---
+++ 7.py
@@ -0,0 +1,10 @@
+def example(arg1,arg2):
+    
+    arg3 = arg1+arg2
+    print(arg3)
+
+x=10
+y=20
+
+output=example(y,x)
+print(output)
